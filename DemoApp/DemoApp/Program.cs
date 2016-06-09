e1b16fc15ff04d665dff61ed0992e5ebbd1c7737
panuganti <rajkiran.panuganti@gmail.com>
2016-06-09 12:03:21 -0700

    Demo App

---
+++ DemoApp/DemoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file1 = args[1];
+            string input2 = args[2];
+            string output = args[3];
+            using (var writer = new StreamWriter(output))
+            {
+                writer.WriteLine("Hello World");
+            }
+        }
+    }
+}
